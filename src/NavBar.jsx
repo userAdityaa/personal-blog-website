ad26174b0bf96f0d0fac25eae9d6a9bdac04acfe
Aditya <aditya.chaudhary1558@gmail.com>
2023-12-17 12:19:30 +0545

    update the previous shadow border to new one

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,11 +2,11 @@ function NavBar(){
     return(
         <nav className="flex items-center bg-[#222222] h-[5rem] w-[100vw] justify-between">
             
-            <div className="h-[100%] w-[19.5%] bg-[#222222]  flex justify-center items-center border border-white shadow-inner shadow-white">
-                <img src='svg-image-1.svg' className="h-[35%]"></img>
+            <div className="h-[100%] w-[19.5%] bg-[#222222]  flex  items-center border border-white shadow-inner-new shadow-white">
+                <img src='svg-image-1.svg' className="h-[35%] ml-[3rem]"></img>
             </div>
             
-            <div className="w-[66%] flex justify-center h-[100%] bg-[#222222] border border-white shadow-inner shadow-white">
+            <div className="w-[66%] flex justify-center h-[100%] bg-[#222222] border border-white shadow-inner-new shadow-white">
                 <ul className="text-white flex items-center w-[60%] h-[100%] justify-around text-[13.96px]  ">
                     <li className="hover:animate-slot">HOME</li>
                     <li className="hover:animate-slot">ABOUT</li>
@@ -19,10 +19,9 @@ function NavBar(){
                 </ul>
             </div>
 
-            <div className="text-white w-[15%] flex justify-center h-[100%] items-center text-[13.96px] bg-[#222222] border border-white shadow-inner shadow-white">
+            <div className="text-white w-[15%] flex justify-center h-[100%] items-center text-[13.96px] bg-[#222222] border border-white shadow-inner-new shadow-white">
                 <p className=" h-[30%] w-[100%] flex justify-center items-center hover:animate-slot">CONTACT</p>
             </div>
-            
         </nav>
     )
 }
